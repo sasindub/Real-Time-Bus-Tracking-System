c79c03d19601a9096292d99fca6f01fb3bf4cf3b
Sasindu Bandara <97359461+sasindub@users.noreply.github.com>
2025-09-29 01:39:45 +0530

    change uri getting from env

--- config/db.js
+++ config/db.js
@@ -1,16 +1,21 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+import mongoose from "mongoose";
 
 const connectDB = async () => {
+  const mongoURI = process.env.MONGO_URI; 
+
+  if (!mongoURI) {
+    console.error("MONGO_URI is not defined!");
+    process.exit(1);
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error(err);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error);
     process.exit(1);
   }
 };
